8a0a64f38315d137fe529559aeee960287db7f46
11georgem <MatthewGeorge2015@u.northwestern.edu>
2016-04-05 19:56:11 -0500

    minor updates

--- src/sys/particles.js
+++ src/sys/particles.js
@@ -1,3 +1,45 @@
 'use strict';
 
 
+// Store for commonly used definitions 
+class Physics {
+    constructor(args){
+	let gravity = args.gravity || -10; 
+	this.gravity = new vec3(0,gravity,0); 
+    }
+
+    get g (){
+	return this.gravity; 
+    }
+}
+
+
+class Particle {
+    constructor(){
+	this.position = vec3(0,0,0);
+	this.velocity = vec3.create();
+	this.acceleration = vec3.create();
+
+	this._damping = 0.998; // Remove energy added by degeneracy
+	this._mass_inverse;
+	this._physics = new Physics; 
+	
+    }
+
+    get p (){
+	return this.position; 
+    }
+
+    get v (){
+	return this.velocity; 
+    }
+
+    get a (){
+	return this.acceleration;
+    }
+    
+}
+
+function map (func, arr){
+    return arr.map(func); 
+}
--- src/sys/vec.js
+++ src/sys/vec.js
@@ -1,29 +1,51 @@
 "use strict";
 // Additional WebGL Utilities
-// Time-stamp: <2016-04-05 14:37:21>
+// Time-stamp: <2016-04-05 19:54:32>
+
 
 class vec3 {
     constructor(x,y,z){
-	this.elements = new Float32Array(3);
-	this.elements[0] = x;
-	this.elements[1] = y;
-	this.elements[2] = z;
+	// Constructor overload 
+	if (typeof x === "object") {
+	    this.elements = x; 
+	} else {
+	    this.elements = new Float32Array(3);
+	    this.elements[0] = x;
+	    this.elements[1] = y;
+	    this.elements[2] = z;
+	}
     }
 
+    get x (){
+	return this.elements[0];
+    }
+
+    get y (){
+	return this.elements[1];
+    }
+
+    get z (){
+	return this.elements[2];
+    }
+
+    get xyz(){
+	return this.elements; 
+    }
+    
     static create(){
 	return new vec3(0,0,0);
     }
 
-    static length(vec){
-	vec = vec.elements;
-	let len = Math.sqrt(vec[0]*vec[0] + vec[1]*vec[1] + vec[2]*vec[2]);
-	return len; 
+    static inverse(vec){
+	return new vec3(map(op('*', -1), vec.xyz));
+    }
+    
+    static magnitude(vec){
+	return Math.sqrt(reduce(function(x,y){return x+y}, map(op('pow', 2), vec.xyz))); 
     }
     
     static normalize(vec){
-	vec = vec.elements; 
-	let len = Math.sqrt(vec[0]*vec[0] + vec[1]*vec[1] + vec[2]*vec[2]);
-	return new vec3(vec[0]/len, vec[1]/len, vec[2]/len);
+	return new vec3(map(op('/', vec3.length(vec)), vec.xyz)); 
     }
     
     static cross(l1,l2){
@@ -51,11 +73,7 @@ class vec3 {
 	return new vec3(tmp[0], tmp[1], tmp[2]);
     }
 
-    static multiply(va, scalar){
-	let tmp = new Float32Array(3);
-	for (let x=0; x<3; x++){
-	    tmp[x] = va.elements[x] * scalar;
-	}
-	return new vec3(tmp[0], tmp[1], tmp[2]);
+    static scale(vec, scalar){
+	return new vec3(map(op('*',scalar), vec.xyz)); 
     }
 }
